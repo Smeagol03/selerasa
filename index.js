5c94213b53ac27fcf66376127debb8b92df28cff
Smeagol03 <129125220+Smeagol03@users.noreply.github.com>
2025-02-09 09:46:52 +0800

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,19 @@ lightbox.option({
   wrapAround: true,
 });
 
+// pesan ke whatsapp
+function sendWhatsAppMessage() {
+  var nama = document.getElementById("exampleInputNama").value;
+  var acara = document.getElementById("exampleInputAcara").value;
+  var alamat = document.getElementById("exampleInputAlamat").value;
+
+  var message = `Halo, Bantu saya menyiapkan makanan untuk acara saya.:%0aNama: ${nama}%0aAcara: ${acara}%0aAlamat: ${alamat}`;
+  var phoneNumber = "+6287894721726";
+  var whatsappUrl = `https://wa.me/${phoneNumber}?text=${message}`;
+
+  window.open(whatsappUrl, "_blank");
+}
+
 // carousel
 var carouselWidth = $(".carousel-inner")[0].scrollWidth;
 var cardWidth = $(".carousel-item").width();
